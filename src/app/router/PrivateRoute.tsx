bb47ef02447b8818eaa47beda8c082164d9fbc00
Montana <a.presovsky@list.ru>
2026-02-13 12:51:25 +0300

    useauth import

--- src/app/router/PrivateRoute.tsx
+++ src/app/router/PrivateRoute.tsx
@@ -3,8 +3,7 @@ import { Navigate, Outlet } from 'react-router';
 import { useAuth } from '@/shared/hooks/useAuthReturn';
 
 const PrivateRoute = (): ReactNode => {
-    // const { isAuthenticated } = useAuth();
-    const isAuthenticated = true
+    const { isAuthenticated } = useAuth();
 
     if (!isAuthenticated) {
         return <Navigate to="/auth" replace />;
